9a38c19924cf51a9c6ea79dfd39bd2fe66dd3172
HALOGENMAN <Shayak.malakar.159@gmail.com>
2019-12-08 18:55:11 +0530

    components

---
+++ react/crwn_clothing/src/App.js
@@ -0,0 +1,15 @@
+import React , { Component } from 'react';
+import HomePage from './pages/homepage/homepage.component'
+
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react/crwn_clothing/src/components/directory/directory.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './directory.style.scss';
+
+class Directory extends React.Component {
+    constructor(){
+        super();
+        this.stste = {};
+    }
+}
+
+export default Directory;
